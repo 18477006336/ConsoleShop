2c4aabb2742a668f4f396abba999818914a58ef0
lenovo <3100216710@aa.com>
2020-12-26 17:13:11 +0800

    excel内容

---
+++ src/User.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class User {
+    private String Username;
+    private String Password;
+    private String Address;
+    private String Phone;
+    List<ReadExcel> ReadExcel;
+
+}
