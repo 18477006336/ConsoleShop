f78bb01286e092d99e15b5b99da9a41b77964b8d
lenovo <3100216710@aa.com>
2020-12-26 16:51:27 +0800

    创建Text.java

--- src/Text.java
+++ src/Text.java
@@ -7,6 +7,7 @@ public class Text {
         String username=sc.next();//阻塞方法
         System.out.println("你输入的用户名："+username);
 
+
         System.out.println("请输入密码：");
         String password=sc.next();
         System.out.println("你输入的密码是："+password);
